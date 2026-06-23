1e180800f0fc542b9bf84a59e76619d944bca3ec
Angel Mitra <angelmitra@Angels-MacBook-Pro.local>
2026-06-23 21:24:17 +0530

    feat: add canine corpus seed queries

---
+++ data_pipeline/config.py
@@ -0,0 +1,12 @@
+SEED_QUERIES = [
+    "dog",
+    "dogs",
+    "canine",
+    "canis familiaris",
+    "dog genetics",
+    "dog cancer",
+    "dog cognition",
+    "dog nutrition",
+    "dog aging",
+    "dog disease",
+]
